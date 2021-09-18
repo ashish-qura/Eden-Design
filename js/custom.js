94cac58cc00d1dbbaaf94974628b6188bf2ca10e
Ashish Makani <57558022+ashish-qura@users.noreply.github.com>
2021-09-19 00:15:47 +0530

    animated input

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+// Testimonial Slider
+
 var menu = ['Viraj Sanghavi', 'Tirth Patel', 'Swapnil Shah']
 var mySwiper = new Swiper ('.swiper-container', {
 	 autoplay: {
@@ -20,52 +22,7 @@ var mySwiper = new Swiper ('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
   })
-
-
-// Animated input text for footer
-timeout_var = null;
-
-function typeWriter(selector_target, text_list, placeholder = false, i = 0, text_list_i=0, delay_ms=200) {
-    if (!i) {
-        if (placeholder) {
-            document.querySelector(selector_target).placeholder = "";
-        }
-        else {
-            document.querySelector(selector_target).innerHTML = "";
-        }
-    }
-    txt = text_list[text_list_i];
-    if (i < txt.length) {
-        if (placeholder) {
-            document.querySelector(selector_target).placeholder += txt.charAt(i);
-        }
-        else {
-            document.querySelector(selector_target).innerHTML += txt.charAt(i);
-        }
-        i++;
-        setTimeout(typeWriter, delay_ms, selector_target, text_list, placeholder, i, text_list_i);
-    }
-    else {
-        text_list_i++;
-        if (typeof text_list[text_list_i] === "undefined")  {
-            setTimeout(typeWriter, (delay_ms*5), selector_target, text_list, placeholder);
-        }
-        else {
-            i = 0;
-            setTimeout(typeWriter, (delay_ms*3), selector_target, text_list, placeholder, i, text_list_i);
-        }
-    }
-}
-
-text_list = [
-    "John Duo"
-];
-
-return_value = typeWriter("#animateInput", text_list, true);
-
-// Animate Input text End
-
-
+// Testimonial Slider End
 
 var app = new Vue({
     el: '#app',
